84001f40b420dd0e3a84588ed5ebbfd53f75abe7
Tatiana Johnson <tatianarjo@gmail.com>
2025-11-11 00:10:47 -0600

    adds sign in and search glass

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
    <div>
    <Button variant="new">Hi Tot Click Me</Button>
-   <Image src="/tottube.svg" height={50} width={50} alt="logo" />
-   <p className= "text-xl font-semibold tracking-tight">Tot Tube</p>
+   {/* <Image src="/tottube.svg" height={50} width={50} alt="logo" />
+   <p className= "text-xl font-semibold tracking-tight">Tot Tube</p> */}
    </div>
   );
 }
--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { SearchInput } from "./search-input"
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { AuthButton } from "@/modules/home/ui/auth/ui/components/auth-button";
 export const HomeNavBar = () => {
     return (
         <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
@@ -12,13 +13,16 @@ export const HomeNavBar = () => {
                     <Link href="/">
                         <div className="p-4 flex items-center gap-1">
                             <Image src="/tottube.svg" height={32} width={32} alt="Tot Tube Logo" />
-                            <p className="text-xl font-semibold tracking-tight">TotTubeGirl</p>
+                            <p className="text-xl font-semibold tracking-tight">TatiTube</p>
                         </div>
                     </Link>
                 </div>
                 {/* Search Bar */}
                 <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
                     <SearchInput />
+                </div>
+                <div className="shrink-0 items-center flex gap-4">
+                    <AuthButton />
                 </div>
             </div>
         </nav>
