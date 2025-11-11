c8c17b9f608aad80804b00867529bc73631a3032
Tatiana Johnson <tatianarjo@gmail.com>
2025-11-11 01:24:45 -0600

    adds sidebar navigation

--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,10 +1,11 @@
-import { Sidebar, SidebarContent, SidebarProvider } from "@/components/ui/sidebar";
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
+import { MainSection } from "./main-section";
 
 export const HomeSidebar = () => {
     return (
         <Sidebar className="pt-16 z-40 border-none">
             <SidebarContent className="bg-background">
-
+                <MainSection />
             </SidebarContent>
         </Sidebar>
     )
