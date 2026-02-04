de67030cca47d00717c844196a5622ab4a379271
Tatiana Johnson <tatianarjo@gmail.com>
2026-02-04 14:45:20 -0600

    adds collapsible function for sidebar

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
    <div>
    {/* <Button variant="new">Hi Tot Click Me</Button> */}
    <Image src="/tottube.svg" height={50} width={50} alt="logo" />
-   <p className= "text-xl font-semibold tracking-tight">Tot Tube</p>
+   <p className= "text-xl font-semibold tracking-tight">I will load videos in the future!</p>
    </div>
   );
 }
